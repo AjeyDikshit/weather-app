0e999034d15f43511b417822b50e83b50035aff4
Ajey Dikshit <dikshitajey@gmail.com>
2024-01-03 21:22:43 +0530

    Requesting data is successful, and the rough webpage is ready

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dhczp6nb3lan39p4pdbxxbfzl3+*j#lygqhjqny)gf7o__&f#2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', ]
 
--- weatherApp/views.py
+++ weatherApp/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render, HttpResponse
 import requests
 
-# access_key = "902c5fbb138e2b150abf57eabff54ac5"
+access_key = "902c5fbb138e2b150abf57eabff54ac5"
+
+
 def home(request):
     # request1 = requests.get(f"http://api.weatherstack.com/current?access_key={access_key}&query=New York")
-    # print(request1.json())
-    return render(request, 'base.html')
+    # data = request1.json()
+    data = dict(request={'type': 'City', 'query': 'New York, United States of America', 'language': 'en', 'unit': 'm'},
+                location={'name': 'New York', 'country': 'United States of America', 'region': 'New York',
+                          'lat': '40.714', 'lon': ' - 74.006', 'timezone_id': 'America / New_York',
+                          'localtime': '2024 - 01 - 03 10: 06', 'localtime_epoch': 1704276360,
+                          'utc_offset': ' - 5.0'},
+                current={'observation_time': '03:06 PM', 'temperature': 2, 'weather_code': 113,
+                         'weather_icons': [
+                             'https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0001_sunny.png'],
+                         'weather_descriptions': ['Sunny'], 'wind_speed': 4, 'wind_degree': 286, 'wind_dir': 'WNW',
+                         'pressure': 1018, 'precip': 0, 'humidity': 62, 'cloudcover': 0, 'feelslike': -2,
+                         'uv_index': 2, 'visibility': 16, 'is_day': 'yes'})
+
+    return render(request, 'base.html', data)
